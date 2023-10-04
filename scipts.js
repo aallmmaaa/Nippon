26ec1f0cfb8ce1fd75f71de64dd3a5fda974fdc8
aallmmaaa <128625322+aallmmaaa@users.noreply.github.com>
2023-10-04 11:22:51 +0200

    Update scipts.js m. hoverkort funktion

--- scipts.js
+++ scipts.js
@@ -10,3 +10,55 @@ function gem(){
     
     document.getElementById("rødkurv").style.display = "none" ;
     document.getElementById("kurv").style.display = "flex" ;}
+
+
+
+
+
+
+document.getElementById("hurtig").addEventListener("mouseenter", vishurtig) ;
+
+function vishurtig(){
+    document.getElementById("hurtig-kort").style.display = "block" ;
+    document.getElementById("klassisk-kort").style.display = "none" ;
+    document.getElementById("eksperimenterende-kort").style.display = "none" ;
+    document.getElementById("økonomisk-kort").style.display = "none" ;}
+
+
+
+document.getElementById("klassisk").addEventListener("mouseenter", visklassisk) ;
+
+function visklassisk(){
+    document.getElementById("klassisk-kort").style.display = "block" ;
+    document.getElementById("hurtig-kort").style.display = "none" ;
+    document.getElementById("eksperimenterende-kort").style.display = "none" ;
+    document.getElementById("økonomisk-kort").style.display = "none" ;}
+
+
+
+document.getElementById("økonomisk").addEventListener("mouseenter", visøkonomisk) ;
+
+function visøkonomisk(){
+    document.getElementById("økonomisk-kort").style.display = "block" ;
+    document.getElementById("eksperimenterende-kort").style.display = "none" ;
+    document.getElementById("klassisk-kort").style.display = "none" ;
+    document.getElementById("hurtig-kort").style.display = "none" ;}
+
+
+
+document.getElementById("eksperimenterende").addEventListener("mouseenter", viseksperimenterende) ;
+
+function viseksperimenterende(){
+    document.getElementById("eksperimenterende-kort").style.display = "block" ;
+    document.getElementById("økonomisk-kort").style.display = "none" ;
+    document.getElementById("klassisk-kort").style.display = "none" ;
+    document.getElementById("hurtig-kort").style.display = "none" ;}
+
+
+document.getElementById("scroll2").addEventListener("mouseleave", gemhover) ;
+
+function gemhover(){
+    document.getElementById("eksperimenterende-kort").style.display = "none" ;
+    document.getElementById("økonomisk-kort").style.display = "none" ;
+    document.getElementById("klassisk-kort").style.display = "none" ;
+    document.getElementById("hurtig-kort").style.display = "none" ;}
